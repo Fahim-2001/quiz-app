709e21bcd3750c4ae68973bb44ef33cbd4c4bf62
unknown <mdyasirofficial2001@gmail.com>
2022-10-11 14:10:33 +0600

    Rechart installed in app

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import { QuizDataContext } from "../../Layout/Main";
 
 const Statistics = () => {
+  const quizCategories = useContext(QuizDataContext);
   return (
     <div>
-      <h1>Statistics </h1>
+      <h1>Statistics {quizCategories.length}</h1>
     </div>
   );
 };
