8be4cbaf4cd79c9ed8a1496f1b31067489f4364f
unknown <mdyasirofficial2001@gmail.com>
2022-10-11 14:39:01 +0600

    Graph added

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,32 @@
 import React, { useContext } from "react";
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { QuizDataContext } from "../../Layout/Main";
 
 const Statistics = () => {
   const quizCategories = useContext(QuizDataContext);
+  // console.log(quizCategories);
   return (
-    <div>
-      <h1>Statistics {quizCategories.length}</h1>
+    <div className="flex flex-col justify-center items-center mt-20 ">
+      <LineChart
+        width={800}
+        height={300}
+        data={quizCategories}
+        margin={{ top: 5, right: 20, bottom: 5, left: 0 }}
+      >
+        <Line type="monotone" dataKey="total" stroke="#8884d8" />
+        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+        <XAxis dataKey="name" />
+        <YAxis dataKey="total" />
+        <Tooltip />
+      </LineChart>
+      <p>A Graph of number of containing questions in every category</p>
     </div>
   );
 };
