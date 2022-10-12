28492a4affa00e10d96e8740dd02d1ab9a36d16e
unknown <mdyasirofficial2001@gmail.com>
2022-10-12 19:49:47 +0600

    Quiz design Complete

--- src/components/QuizShow/QuizShow.js
+++ src/components/QuizShow/QuizShow.js
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const QuizShow = ({ quiz }) => {
   const { question, options, correctAnswer } = quiz;
+  // const [ans, setAns] = useState();
+  // console.log(ans);
+  // if (ans === correctAnswer) {
+  //   console.log("Correct!");
+  // } else {
+  //   console.log("Wrong!");
+  // }
+  const setAns = (ans) => {
+    console.log(ans);
+    if (ans === correctAnswer) {
+      toast.success("Correct!", { autoClose: 750 });
+    } else {
+      toast.warning("Wrong Answer", { autoClose: 750 });
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center m-4 border-2 bg-gradient-to-r from-cyan-500 to-blue-500  shadow-lg dark:bg-gray-800 w-2/4 rounded-md text-white">
       <h1 className="text-white text-md font-medium m-2">{question}</h1>
-      <div className="grid grid-cols-2 gap-2 ">
-        <label htmlFor="1">
-          <p>
-            <input type="radio" name="answer" id="1" value={options[0]} />{" "}
-            {options[0]}
-          </p>
-        </label>
-        <label htmlFor="2">
-          <p>
-            <input type="radio" name="answer" id="2" value={options[1]} />{" "}
-            {options[1]}
-          </p>
-        </label>
-        <label htmlFor="3">
-          <p>
-            <input type="radio" name="answer" id="3" value={options[2]} />{" "}
-            {options[2]}
-          </p>
-        </label>
-        <label htmlFor="4">
-          <p>
-            <input type="radio" name="answer" id="4" value={options[3]} />{" "}
-            {options[3] ? options[3] : "Above All"}
-          </p>
-        </label>
+      <div className="grid grid-cols-2 gap-2 m-4">
+        <button
+          className="focus:bg-slate-600 rounded-md"
+          onClick={() => setAns(options[0])}
+        >
+          1.{options[0]}
+        </button>
+        <button
+          className="focus:bg-slate-600 rounded-md"
+          onClick={() => setAns(options[1])}
+        >
+          2.{options[1]}
+        </button>
+        <button
+          className="focus:bg-slate-600 rounded-md"
+          onClick={() => setAns(options[2])}
+        >
+          3.{options[2]}
+        </button>
+        <button
+          className="focus:bg-slate-600 rounded-md"
+          onClick={() => setAns(options[3])}
+        >
+          4.{options[3]}
+        </button>
       </div>
     </div>
   );
--- src/components/Quizes/Quizes.js
+++ src/components/Quizes/Quizes.js
@@ -4,15 +4,35 @@ import QuizShow from "../QuizShow/QuizShow";
 
 const Quizes = () => {
   const quizes = useLoaderData();
-  // const quizInfo = quizes.data;
+  // console.log(quizes);
+  const quizInfo = quizes.data;
   const quizData = quizes.data.questions;
-  console.log(quizData);
+  // console.log(quizData);
 
   return (
-    <div className="flex flex-col justify-center items-center m-4">
-      {quizData.map((quiz) => (
-        <QuizShow key={quiz.id} quiz={quiz}></QuizShow>
-      ))}
+    <div>
+      <div className="flex justify-center items-center mt-4">
+        <img
+          src={quizInfo.logo}
+          alt=""
+          height={"50px"}
+          width={"50px"}
+          className="bg-black rounded-md mr-6"
+        />
+        <p className="text-2xl font-semibold">{quizInfo.name}</p>
+      </div>
+      <div className="text-xl font-medium">
+        <p>
+          In this cateory you have to answer
+          <span className="font-bold text-2xl ml-2">{quizInfo.total}</span>{" "}
+          Quizes
+        </p>
+      </div>
+      <div className="flex flex-col justify-center items-center m-4">
+        {quizData.map((quiz) => (
+          <QuizShow key={quiz.id} quiz={quiz}></QuizShow>
+        ))}
+      </div>
     </div>
   );
 };
